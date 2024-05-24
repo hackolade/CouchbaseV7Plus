34e70afb7808688dbe94d812df78bf7314c6c397
Serhii Filonenko <serhii.filonenko@binary-studio.com>
2024-05-24 18:26:53 +0300

    HCK-6081: fix generate keys by sample if key has changed name

--- forward_engineering/services/statements/insertStatements.js
+++ forward_engineering/services/statements/insertStatements.js
@@ -35,9 +35,13 @@ const getInsertScriptForCollection = ({ jsonData, collection }) => {
 	);
 
 	const insertionPath = getKeySpaceReference(collection);
+	const isKeyGeneratedWithFakerFunction = collection?.properties?.[keyPropertyName]?.fakerFunction;
 	const parseJsonData = JSON.parse(jsonData);
+	const sampleValue = collection?.properties?.[keyPropertyName]?.sample;
+	const keyValue = isKeyGeneratedWithFakerFunction ? parseJsonData[keyPropertyName] : sampleValue;
 	const { [keyPropertyName]: keyProperty, ...jsonDataBody } = parseJsonData;
-	const sampledKey = getKeyFieldSample(parseJsonData[keyPropertyName]);
+
+	const sampledKey = getKeyFieldSample(keyValue);
 
 	return `INSERT INTO ${insertionPath} (KEY, VALUE)\n\tVALUES("${sampledKey}",${JSON.stringify(jsonDataBody, null, '\t')});`;
 };
